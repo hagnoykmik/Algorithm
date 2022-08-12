9930a38e87cb5f6a2ad6ef44ad0350acd51a6a41
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2022-08-12 10:59:48 +0900

    [Bronze II] Title: 태보태보 총난타, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/17249. 태보태보 총난타/태보태보 총난타.py
@@ -0,0 +1,18 @@
+tae_bo = input()
+
+left = 0
+right = 0
+
+for char in tae_bo:
+    if char == '@':
+        left += 1
+    elif char == '(':
+        break
+
+for char in tae_bo[::-1]:
+    if char == '@':
+        right += 1
+    elif char == ')':
+        break
+
+print(left, right)
