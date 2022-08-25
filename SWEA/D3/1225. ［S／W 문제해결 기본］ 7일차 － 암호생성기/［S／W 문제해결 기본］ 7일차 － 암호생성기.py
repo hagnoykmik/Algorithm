d1c91c7e2fda59c811ef7e89911d0f04aaeedcf2
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2022-08-25 11:15:02 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 161 ms, Memory: 59,876 KB -BaekjoonHub

---
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -0,0 +1,16 @@
+for tc in range(1, 11):
+    t = int(input())
+    queue = list(map(int, input().split()))
+
+    while queue[-1] != 0:           # 큐의 마지막값이 0이 되면 멈춘다
+        for i in range(1, 6):       # 5번의 사이클 
+            p = queue.pop(0)
+                                    # p-i가 0보다 클 때
+            if p > i:               
+                queue.append(p - i)
+
+            else:                   # p-i가 0보다 작을 때
+                queue.append(0)
+                break               
+
+    print(f'#{tc}', *queue)
