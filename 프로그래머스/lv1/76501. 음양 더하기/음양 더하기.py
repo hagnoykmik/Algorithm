f965d420d572a4cf2fe8a106d92b8d86d8ab3e48
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2022-10-07 23:31:00 +0900

    [level 1] Title: 음양 더하기, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,11 @@
+def solution(absolutes, signs):
+    answer = 0
+    n = len(absolutes)
+    for i in range(n):
+        if signs[i]:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+        
+    
+    return answer
