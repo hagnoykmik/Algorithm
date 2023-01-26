81725d0e5fec1e3286b7870915f72c6e086f29c4
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-01-26 23:24:29 +0900

    [Bronze V] Title: 영수증, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,11 @@
+X = int(input())
+n = int(input())
+price = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    price += a * b
+if X == price:
+    print('Yes')
+else:
+    print('No')
