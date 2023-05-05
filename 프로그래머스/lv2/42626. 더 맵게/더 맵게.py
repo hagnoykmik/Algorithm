9767f01f624c0334432b12e9265b5f6e357813aa
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-05-05 23:23:25 +0900

    Update 더 맵게.py

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -7,14 +7,13 @@ def solution(scoville, K):
     while scoville[0] < K and len(scoville) >= 2:   
     # 맨 앞의 숫자가 K보다 크면 그 리스트의 모든 수는 K보다 크다 
 
-        a = heapq.heappop(scoville)
+        a = heapq.heappop(scoville)         # que는 선입선출
         b = heapq.heappop(scoville)
 
         heapq.heappush(scoville, a + b * 2) # heap은 정렬안해도 됨
 
         answer += 1   
 
-    # print(scoville[0])
     if scoville[0] < K:
         
         answer = -1
