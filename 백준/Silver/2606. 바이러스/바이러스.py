d41ac8a56a048b452ebe775b70c86c6bbe32a05e
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-05-24 17:14:54 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,16 +1,13 @@
 def dfs(v):
-    visited[v] = True  # 방문처리
-    stack = [v]   
+    visited[v] = True
+    stack = [v]
 
     while stack:
-        for next_v in graph[v]:         # 인접한 정점 다 방문처리
-            if not visited[next_v]:     # 아직 방문안했으면
-                stack.append(v)
-                v = next_v
-                visited[v] = True
-                break
-        else:                           # 더이상 방문할 곳이 없다면
-            v = stack.pop()
+        v = stack.pop()
+        for nx in graph[v]:
+            if not visited[nx]:
+                visited[nx] = True
+                stack.append(nx)
 
 
 n = int(input())
