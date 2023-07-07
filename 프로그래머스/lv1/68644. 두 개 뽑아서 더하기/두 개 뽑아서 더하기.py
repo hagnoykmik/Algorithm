0c1eb8a97b99df527119c15a690bcd479295d861
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-07-07 09:26:34 +0900

    Update 두 개 뽑아서 더하기.py

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -5,4 +5,4 @@ def solution(numbers):
         for j in range(i+1, len(numbers)):
             answer.add(numbers[i] + numbers[j])        
         
-    return sorted(list(answer))
+    return sorted(list(answer))  # TypeError: Object of type set is not JSON serializable가 나면 list로 감싸줘야 제대로 인식
