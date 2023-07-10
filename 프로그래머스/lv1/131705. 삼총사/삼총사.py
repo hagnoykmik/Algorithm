e4d8ed68da2a223a4d1e7fe9a7f1128409e72ef7
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-07-10 09:21:05 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -3,7 +3,7 @@ from itertools import combinations  # 부분집합 메서드 사용
 def solution(number):
     answer = 0
     
-    for students in list(combinations(number, 3)): #combinations 메서드를 사용하면 객체 형태이기 때문에 리스트에 형변환해서 담아줘야 한다
+    for students in combinations(number, 3): #combinations 메서드를 사용하면 객체 형태이기 때문에 리스트에 형변환해서 담아줘야 한다 -> 안담아줘도 튜플로 잘 나온다
         if sum(students) == 0:
             answer += 1
     
