47f139b03e50539fc76b635527a3b861f550cdbf
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-07-12 09:34:10 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -4,10 +4,9 @@ def solution(k, score):
     for day in range(len(score)):
         if not top3 or day <= k-1:   # 명예의 전당이 비어있거나, k-1일 이하(인덱스는 0부터라서)이면
             top3.append(score[day])  # 무조건 넣는다
-            top3.sort()              # 오름차순으로 정렬(0번째 값과 비교하기 위해)
         elif top3[0] < score[day]:   # 그렇지 않으면 k번째 점수와 비교한다
             top3.pop(0)              # 그날의 점수가 top3의 점수보다 크면 교체한다
             top3.append(score[day])
-            top3.sort()
+        top3.sort()              # 오름차순으로 정렬(0번째 값과 비교하기 위해)
         answer.append(top3[0])   
     return answer
