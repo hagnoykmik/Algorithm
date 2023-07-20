4d0f33b5feabc717b580fec7c25416cff6de3668
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-07-20 13:24:00 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.13 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,7 +1,5 @@
 def solution(s):
-    answer = []
-    num = list(map(int, s.split()))
-    answer.append(str(min(num)))
-    answer.append(str(max(num)))
-    new = ' '.join(answer)
-    return new
+    num_list = list(map(int, s.split(" "))) # 리스트를 감싸줘야 한다(안그러면 map타입으로 반환)
+    answer = (lambda x, y: x + " " + y)(str(min(num_list)), str(max(num_list)))
+    return answer
+
