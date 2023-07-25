7ef3267fb9171fe9cea88728efc9674751b0b737
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-07-25 14:37:40 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.73 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,10 +1,10 @@
 def solution(A,B):
     answer = 0
-
-    A.sort()
-    B.sort(reverse=True)
-
-    for i in range(len(A)):
-        answer += A[i] * B[i]
+    num_list = list(zip(sorted(A), sorted(B, reverse=True)))
+    print(num_list)
     
+    num = lambda a, b: a * b
+    for a, b in num_list:
+        answer += num(a, b)
+        
     return answer
