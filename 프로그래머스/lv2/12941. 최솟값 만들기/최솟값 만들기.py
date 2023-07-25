08c8066f4477de70685d95585fd3e0f537e3ffd4
hagnoykmik <ymj02148@naver.com>
2023-07-25 14:44:50 +0900

    [level 2] Title: 최솟값 만들기

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,10 +1,4 @@
 def solution(A,B):
-    answer = 0
-    num_list = list(zip(sorted(A), sorted(B, reverse=True)))
-    print(num_list)
-    
-    num = lambda a, b: a * b
-    for a, b in num_list:
-        answer += num(a, b)
-        
+    num_list = list(zip(sorted(A), sorted(B, reverse=True))) # 곱할 값을 하나로 묶기
+    answer = sum(a * b for a, b in num_list)                 # 람다함수로 굳이 만들어 주지 않아도 됨
     return answer
