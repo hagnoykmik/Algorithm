819049355405210ffbad42ea1c9bef5a6306a9b0
hagnoykmik <ymj02148@naver.com>
2023-07-25 09:36:58 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -1,13 +1,3 @@
 def solution(strings, n):
-    answer = []
-    
-    # n번째 글자와 문자열을 같이 저장
-    for i in range(len(strings)):
-        strings[i] = (strings[i], ord(strings[i][n]))
-    # n번째 글자기준(같으면 사전순)으로 정렬
-    strings.sort(key=lambda c: (c[1], c[0])) 
-    # answer에 담기    
-    for word in strings:
-        answer.append(word[0])
-    
-    return answer
+    strings.sort(key=lambda c: (c[n],c))  # n번째 글자로 정렬(같으면 사전순정렬))
+    return strings
