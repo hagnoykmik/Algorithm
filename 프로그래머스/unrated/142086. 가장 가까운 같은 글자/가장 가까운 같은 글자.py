11022a33947231a29017672bf804ccbceadf87ec
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-07-26 09:31:17 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 4.20 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -5,10 +5,9 @@ def solution(s):
         # 찾는 값이 없을 때
         if word.rfind(c) == -1: # 인덱스 중에 가장 큰 값을 찾아준다
             answer.append(-1)
-            word += c
         # 찾는 값이 있을 때
         else:
             answer.append(i - word.rfind(c))
-            word += c
+        word += c
             
     return answer
