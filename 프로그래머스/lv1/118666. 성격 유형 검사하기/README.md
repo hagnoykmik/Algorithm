ed80114412ec9b8b2cdff2558b4e1dc914c1b696
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-07-27 16:06:57 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.24 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/README.md
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 성격 유형 검사하기 - 118666 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.35 ms
+메모리: 10.2 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,38 +1,20 @@
 def solution(survey, choices):
-    personality = {"R": 0, "T": 0, "C": 0, "F": 0, "J": 0, "M": 0, "A": 0, "N": 0}
-    scores = [0, 3, 2, 1, 0, 1, 2, 3]
-    # 1이면 survey[i][0] 에 +=3
-    # 2이면 survey[i][0] 에 +=2
-    # 3이면 survey[i][0] 에 +=1
-    # 4이면 += 0
-    # 5이면 survey[i][1] 에 +=1
-    # 6이면 survey[i][1] 에 +=2
-    # 7이면 survey[i][1] 에 +=3
-
-    n = len(survey)
     answer = ''
-    p_list = []  # 성격유형 담기
-    s_list = []  # 성격유형 점수 담기
-
-
-    # i번째의 0번째 캐릭터가 비동의(1,2,3), 1번째가 동의(5,6,7) 캐릭터
-    for i in range(n):
-        if choices[i] <= 3:
-            personality[survey[i][0]] += scores[choices[i]]
-        elif choices[i] >= 5:
-            personality[survey[i][1]] += scores[choices[i]]
-
-
-    # 성격유형검사
-    for p in personality:
-        p_list.append(p)
-        s_list.append(personality[p])
-
-    for j in range(4):
-        if s_list[2 * j] >= s_list[2 * j + 1]:
-            answer += p_list[2 * j]
+    score_d = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0} #점수표
+    
+    for q, score in zip(survey, choices):
+        if score < 4:
+            score_d[q[0]] += 4 - score # 앞의 지표에 점수주기
+        elif score > 4:
+            score_d[q[1]] += score - 4 # 뒤의 지표에 점수주기
+    
+    score_l = list(score_d.items())    # 딕셔너리 -> 리스트
+    
+    for i in range(0, len(score_l)-1, 2):
+        if score_l[i][1] >= score_l[i+1][1]:
+            answer += score_l[i][0]
         else:
-            answer += p_list[2 * j + 1]
-
-    print(f'"{answer}"')
+            answer += score_l[i+1][0]
+    
     return answer
+    
