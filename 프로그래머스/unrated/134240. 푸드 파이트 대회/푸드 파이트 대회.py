924936efcda6664309dfa039e0282897e0ebac70
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-07-27 15:04:02 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,12 +1,10 @@
 def solution(food):
-    answer = ''
+    first = ''
     for n, f in enumerate(food):
         # n번째 음식을 f//2번 먹어야한다
-        for _ in range(int(f)//2):
-            answer += str(n)
-    answer += '0' # 물
+        first += str(n) * (int(f)//2)
+        
     # 물을 제외한 음식 거꾸로 붙이기
-    for i in range(len(answer)-2, -1, -1):
-        answer += answer[i]
-    
+    second = first[::-1]
+    answer = first + '0' + second
     return answer
