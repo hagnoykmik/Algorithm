c669df369a530ca94c5e03595b9c31b7a4fbef0c
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-07-28 09:45:14 +0900

    [level 2] Title: 주식가격, Time: 50.61 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -2,7 +2,7 @@ def solution(prices):
     answer = []
     
     for i, p in enumerate(prices):
-        for j in range(i, len(prices)): # 내 다음부터 가격 보기
+        for j in range(i+1, len(prices)): # 내 다음부터 가격 보기
             if p > prices[j]:           # 내가격보다 떨어지면 몇초동안 떨어졌는지 확인 후 break
                 answer.append(j-i)
                 break
