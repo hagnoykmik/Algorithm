26c9afe7d15aa00fa43a434b4fc6f8016f8928bb
hagnoykmik <ymj02148@naver.com>
2023-08-01 13:26:16 +0900

    카펫_더 나은 코드

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,9 +1,8 @@
 def solution(brown, yellow):
     answer = []
     l = brown + yellow
-    for i in range(1, yellow+1):
+    for i in range(1, int(yellow**(1/2))+1): #루트n까지만 구해야 약수를 중복되지 않게 구한다.
         if yellow % i == 0:
             if (i+2) * (yellow//i+2) == l:
-                answer.append((i+2, yellow//i+2))
-        
-    return answer[-1]
+                answer = [yellow//i+2, i+2] # 큰 수를 먼저 넣는다
+    return answer
