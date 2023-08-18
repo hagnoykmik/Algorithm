61194e8c6807a356869345e6287df692cc8355f8
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-08-18 13:53:12 +0900

    [level 1] Title: 소수 찾기, Time: 97.40 ms, Memory: 24 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,11 +1,13 @@
 def solution(n):
     answer = 0
-    for num in range(2, n+1):
-        # 약수를 반만 찾는다 (num ** 1/2까지만 찾으면 뒤에는 반전)
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                break
-        else:
-            answer += 1
-                
-    return answer
+
+    # 에라토스테네스의 체
+    num_list = [True] * (n + 1)
+    
+    num = int(n ** 0.5)
+    for i in range(2, num + 1):
+        if num_list[i] == True:
+            # 소수의 배수들을 지운다
+            for j in range(i + i, n + 1, i):
+                num_list[j] = False
+    return num_list[2:].count(True)
