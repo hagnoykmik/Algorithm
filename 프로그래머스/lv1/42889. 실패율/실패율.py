1563463062c69a417b1a18515a1e4b8a16d48fd8
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-08-21 23:32:47 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,3 +1,21 @@
 def solution(N, stages):
-    answer = []
+    stages.sort()      # 1번부터 정렬
+    failure_list = []  # 실패율 리스트
+    user = len(stages) # 사용자 수
+    
+    for stage in range(1, N+1):
+        # 해당 스테이지에 도달한 플레이어 수
+        players_on_stage = stages.count(stage)
+        
+        if user == 0:
+            failure_list.append((stage, 0))
+        else:
+            failure_list.append((stage, players_on_stage / user)) # 실패율을 구해서 리스트에 넣고))
+            user -= players_on_stage  # 스테이지 제거
+            
+            
+    # 내림차순으로 정렬
+    failure_list.sort(reverse=True, key=lambda x: x[1])
+    answer = [stage for stage, _ in failure_list]
+        
     return answer
