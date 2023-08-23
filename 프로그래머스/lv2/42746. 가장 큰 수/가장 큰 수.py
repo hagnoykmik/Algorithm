415d383bd8667ddb3f84a76a8c9c71c352667d27
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-08-23 16:48:24 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,12 +1,3 @@
 def solution(numbers):
-
-    numbers = list(map(str, numbers))
-    numbers.sort(reverse=True, key=lambda x: (x * 4)[:4])
-    # 3434,3333,3030
-    
-    answer = ''.join(numbers)
-    
-    if answer.count('0') == len(answer):
-        return '0'
-    else:
-        return answer
+    answer = ''
+    return answer
