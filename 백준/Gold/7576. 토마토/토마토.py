f1efe62937c536a0699df08bc16119cf08cf0571
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-10-22 16:26:53 +0900

    [Gold V] Title: 토마토, Time: 1236 ms, Memory: 98576 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,46 +1,46 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-# 인접한 토마토 찾기
-def search(x, y, tomato):
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+def tomato(q):
+    while q:
+        r, c, day = q.popleft()
 
-        if 0 <= nx < m and 0 <= ny < n and box[nx][ny] == 0:
-            box[nx][ny] = 1                                     # 인접한 토마토 익히기
-            tomato.append((nx, ny))                             # 다음 날 익을 토마토의 좌표를 튜플로 리스트에 담기
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
 
+            # 이동가능한 범위이고 익지 않은 토마토라면
+            if 0 <= nr < n and 0 <= nc < m and box[nr][nc] == 0:
+                q.append((nr, nc, day + 1))
+                box[nr][nc] = 1
 
-# 상, 하, 좌, 우 (익힐 수 있는 토마토)
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-
-n, m = map(int, input().split())
-box = [list(map(int, input().split())) for _ in range(m)]
-day = -1              # 첫 날 익은 토마토 값을 넣고 시작할 것이기 때문에
-tomato = deque()      # 다음날 익을 토마토 담을 리스트
+    return day
 
 
-# 익은 토마토 위치 담기
-for x, list_box in enumerate(box):
-    for y in range(n):
-        if list_box[y] == 1:
-            tomato.append((x, y))
+m, n = map(int, input().split())
+box = [list(map(int, input().split())) for _ in range(n)]
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+q = deque([])
 
 
-while tomato:
-    cnt = len(tomato)                  # 같은 날 익은 토마토를 체크하기 위함
-    for j in range(cnt):
-        result = tomato.popleft()
-        x, y = result[0], result[-1]
-        search(x, y, tomato)
-    day += 1                           # 다 담으면 다음날이 됨
+# 익은 토마토(1) 찾기
+for r in range(n):
+    for c in range(m):
+        if box[r][c] == 1:
+            q.append((r, c, 0))
 
+if q:
+    result = tomato(q)
 
-for check in box:     # 안 익은 토마토가 있다면 -1을 출력
-    if 0 in check:
-        day = -1
-
-print(day)
-
+    # q가 비어있으면 토마토 익힐 수 없으니까 바로 -1 출력
+    for idx, line in enumerate(box):
+        if line.count(0) > 0:
+            print(-1)
+            break
+    # 아니라면 토마토 익히러 가기
+    else:   
+        print(result)
+else:
+    print(-1)
