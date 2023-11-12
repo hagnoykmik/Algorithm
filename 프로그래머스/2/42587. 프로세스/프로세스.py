6556015bfb5302adf7e059ccfb7f12e5aff0e64b
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-11-12 17:52:08 +0900

    [level 2] Title: 프로세스, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,20 @@
+from collections import deque
+def solution(priorities, location):
+    answer = 0
+    priorities_sort = sorted(priorities, reverse=-1) # 우선순위 높은 것부터 정렬
+    priorities_q = deque((i, p) for i, p in enumerate(priorities))
+    run = [] # 실행시킨 순서
+    
+    while priorities_q:
+        idx, priority = priorities_q.popleft()
+        
+        # 자신보다 우선순위가 높은 프로세스가 있을 때
+        if priority != priorities_sort[0]:
+            priorities_q.append((idx, priority))
+            
+        # 자기가 제일 우선순위 높을 때
+        else:
+            run.append(priorities_sort.pop(0))
+            if idx == location:
+                return len(run)
+        
