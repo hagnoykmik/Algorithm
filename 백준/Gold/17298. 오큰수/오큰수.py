20f374a65c162b9b6cbd9cf9bf4be57806a17d96
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-11-21 16:21:25 +0900

    [Gold IV] Title: 오큰수, Time: 1212 ms, Memory: 142240 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+sequence = deque(map(int, input().split()))
+stack = []
+result = [] # 자신보다 큰 수 담는 리스트
+
+
+while sequence:
+    # 진행방향 거꾸로 검사한다
+    number = sequence.pop()
+
+    # 자기보다 같거나 작으면 stack에서 제거
+    while stack and stack[-1] <= number:
+        stack.pop()
+
+    # 자기 보다 큰 수 중 제일 왼쪽(위쪽)에 있는 수 담기
+    if stack:
+        result.append(stack[-1])
+    else:
+        result.append(-1)
+    stack.append(number)
+
+result.reverse() # 다시 진행방향으로 바꿔주기
+
+print(*result)
