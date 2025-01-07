1d910ba992ce66d3e0b58ad5ef163d926a032f35
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2025-01-07 16:18:20 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 188 ms, Memory: 17256 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,13 +1,11 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     // 단어 뒤집는 메서드
-    public static void reverse(Stack<Character> stack) {
+    public static void reverse(BufferedWriter bw, Stack<Character> stack) throws IOException {
         while (!stack.isEmpty()) {
-            System.out.print(stack.pop());
+            bw.write(stack.pop());
         }
     }
 
@@ -16,28 +14,30 @@ public class Main {
         String S = br.readLine();
         boolean tag = false;
         Stack<Character> stack = new Stack<>();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         for (char ch : S.toCharArray()) {
             // 태그 시작일 때
             if (ch == '<') {
                 // 태그전에 있던 단어 뒤집어줘야한다
-                reverse(stack);
+                reverse(bw, stack);
                 tag = true;
-                System.out.print(ch);
+                bw.write(ch);
             } else if (ch == '>') { // 태그 끝일 때
                 tag = false;
-                System.out.print(ch);
+                bw.write(ch);
             } else if (tag) { // 태그안의 알파벳일 때
-                System.out.print(ch);
+                bw.write(ch);
             } else {
                 if (ch == ' ' || ch == '\n') { // 공백일때는 뒤집어준다
-                    reverse(stack);
-                    System.out.print(ch);
+                    reverse(bw, stack);
+                    bw.write(ch);
                 } else { // 단어일 때는 스택에 넣는다
                     stack.push(ch);
                 }
             }
         }
-        reverse(stack);
+        reverse(bw, stack);
+        bw.flush();
     }
 }
