5278cd987657acaee64407aaf1865edd9c916baf
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2025-01-13 18:11:57 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -4,30 +4,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        long [][] d = new long[n+1][2];
-        long ans = 0;
+        long[] d = new long[n+1];
         
-        // 길이가 0일 때
-        d[1][0] = 0; // 0으로 시작X
-        d[1][1] = 1;
+        // d[n] = n자리 이친수 개수
+        // 마지막 수가 0일 때 -> d[n-1]
+        // 마지막 수가 1일 때 -> d[n-2]
+        d[0] = 0;
+        d[1] = 1;
         
         for (int i = 2; i <= n; i++) {
-            for (int l = 0; l <= 1; l++) {
-                // 마지막 자리수가 0일 때
-                if (l == 0) {
-                    d[i][0] = d[i-1][0] + d[i-1][1];
-                }
-        
-                // 마지막 자리수가 1일 때
-                if (l == 1) {
-                    d[i][1] = d[i-1][0];
-                }
-            }
+            d[i] = d[i-1] + d[i-2];
         }
-        
-        for (int i = 0; i <= 1; i++) {
-            ans += d[n][i];
-        }
-        System.out.print(ans);        
+        System.out.print(d[n]);
     }
 }
