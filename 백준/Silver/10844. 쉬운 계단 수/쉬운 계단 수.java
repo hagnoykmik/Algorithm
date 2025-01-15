fbda1689d69455a69291fa67bde173c969ccb8fa
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2025-01-15 22:49:07 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -3,6 +3,7 @@ import java.io.*;
 public class Main {
     public static long mod = 1000000000L;
     public static void main(String[] args) throws IOException {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         long[][] d = new long[101][10];
