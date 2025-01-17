d2f7e2c0bb71efddfd2cb326664080b07fb81750
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2025-01-17 13:53:48 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14664 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        int[][] a = new int[n+1][3]; // 각 색으로 칠하는 가격
+        for (int i = 1; i < n+1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                a[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int[][] d = new int[n+1][3]; // 최소값 저장
+        for (int i = 1; i < n+1; i++) {
+            d[i][0] = Math.min(d[i-1][1], d[i-1][2]) + a[i][0];
+            d[i][1] = Math.min(d[i-1][0], d[i-1][2]) + a[i][1];
+            d[i][2] = Math.min(d[i-1][0], d[i-1][1]) + a[i][2];
+        }
+        
+        System.out.print(Math.min(Math.min(d[n][0], d[n][1]), d[n][2]));        
+    }
+}
