7b432d2822dcc89cabee580a2ab17d87e4b00b5f
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2025-03-26 00:20:32 +0900

    [level 0] Title: 옹알이 (1), Time: 0.88 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,20 +1,18 @@
 class Solution {
     public int solution(String[] babbling) {
-        String[] str = {"aya", "ye", "woo", "ma"};
         int answer = 0;
-        
-        for (int i = 0; i < babbling.length; i++) {
-            for (int j = 0; j < str.length; j++) {
-                // 조카가 할 수 있는 옹알이면 " "공백으로 변환
-                babbling[i] = babbling[i].replace(str[j], " ");
-            }
-            
-            // 공백제거했을 때 다 없어지면 발음 가능
-            if (babbling[i].trim().length() == 0) {
-                answer += 1;
+
+        for(int i =0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].replace("aya", "1");
+            babbling[i] = babbling[i].replace("woo", "1");
+            babbling[i] = babbling[i].replace("ye", "1");
+            babbling[i] = babbling[i].replace("ma", "1");
+            babbling[i] = babbling[i].replace("1", "");
+            if(babbling[i].isEmpty()) {
+                answer = answer + 1;
             }
         }
-        
+
         return answer;
     }
 }
