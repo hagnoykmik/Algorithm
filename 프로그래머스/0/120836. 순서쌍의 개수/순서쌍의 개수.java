a5500e7e9db5deadd2d2d94bbf250dc3d6e092b9
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2025-04-10 17:54:47 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -3,11 +3,11 @@ class Solution {
         int answer = 0;
         for (int i = 1; i * i <= n; i++) {
             if (n % i == 0) {
-                if (i * i == n) {
+                if (i * i == n) { // 제곱이면 +1만 한다(1쌍만 나옴)
                     answer += 1;
                     continue;
                 };
-                answer += 2;
+                answer += 2; // 아닐땐 2쌍이 나옴
             }
         }
         return answer;
