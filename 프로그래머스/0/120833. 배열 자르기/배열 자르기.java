80d4da9b45612eb45be040cf8cd4b318ccccdef5
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2025-04-10 15:57:16 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
+        // copyOfRange(배열, 시작인덱스, 마지막인덱스 앞요소까지만 복사)
         int[] answer = Arrays.copyOfRange(numbers, num1, num2+1);
         return answer;
     }
