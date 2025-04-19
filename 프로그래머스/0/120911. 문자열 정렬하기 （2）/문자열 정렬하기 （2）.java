e9b55126f1c55ea279a62e62e7025b90f0246531
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2025-04-19 15:43:47 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.50 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -4,7 +4,6 @@ class Solution {
     public String solution(String my_string) {
         char[] chars = my_string.toLowerCase().toCharArray(); // 배열로 변환하면서 소문자로 변환
         Arrays.sort(chars); // 알파벳순으로 정렬
-        String answer = new String(chars); // 문자열로 변환
-        return answer;
+        return new String(chars);
     }
 }
