0b53c1387485979d4403a43acb9774e37d927088
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2025-04-29 22:30:57 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 19.33 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,6 +4,7 @@ import java.util.*;
 아이디어: wires를 순회하면서 순서대로 한개씩 제외하고 bfs해서 덩어리 구하기
 */
 class Solution {
+    // 덩어리 구하기 메서드
     public static int bfs (int start, boolean[] check, ArrayList<Integer>[] a, int cnt) {
         ArrayDeque<Integer> q = new ArrayDeque<>();
         q.add(start); // 시작점을 큐에 넣고
@@ -23,7 +24,7 @@ class Solution {
     }
     
     public int solution(int n, int[][] wires) {
-        int answer = 100;
+        int answer = n;
         int l = wires.length;
         
         // n번 순회하면서 0번 부터 n-1번까지 한번씩 제외해보기
@@ -45,14 +46,8 @@ class Solution {
             
             // 2. bfs 탐색으로 덩어리 구하기
             boolean[] check = new boolean[n+1]; // 방문체크
-            ArrayList<Integer> d = new ArrayList<>();
-            for (int start = 1; start <= n; start++) {
-                if (!check[start]) {
-                    int top = bfs(start, check, a, 1);
-                    d.add(top);
-                }
-            }
-            answer = Math.min(answer, Math.abs(d.get(0)-d.get(1)));
+            int top = bfs(1, check, a, 1);
+            answer = Math.min(answer, Math.abs(n-top-top));
         }
         return answer;
     }
