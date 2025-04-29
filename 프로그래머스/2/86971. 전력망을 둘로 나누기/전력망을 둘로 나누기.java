f01cd4264644e84933661fa5c925ed14aa705faa
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2025-04-29 21:57:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 34.80 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+/*
+아이디어: wires를 순회하면서 순서대로 한개씩 제외하고 bfs해서 덩어리 구하기
+*/
+class Solution {
+    public static int bfs (int start, boolean[] check, ArrayList<Integer>[] a, int cnt) {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.add(start); // 시작점을 큐에 넣고
+        check[start] = true; // 방문 처리
+        
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            for (int nx : a[x]) {
+                if (!check[nx]) {
+                    q.add(nx);
+                    check[nx] = true;
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+    
+    public int solution(int n, int[][] wires) {
+        int answer = 100;
+        int l = wires.length;
+        
+        // n번 순회하면서 0번 부터 n-1번까지 한번씩 제외해보기
+        for (int i = 0; i < l; i++) {
+            // 0. 인접리스트 만들기
+            ArrayList<Integer>[] a = new ArrayList[n+1];
+            for (int j = 0; j <= n; j++) {
+                a[j] = new ArrayList<>();
+            }
+            
+            // 1. 인접리스트 채우기
+            for (int j = 0; j < l; j++) {
+                if (j != i) { // 0번째부터 n-1번째까지 한개씩 끊어보기
+                    // 인접 리스트 만들기
+                    a[wires[j][0]].add(wires[j][1]);
+                    a[wires[j][1]].add(wires[j][0]);
+                }
+            }
+            
+            // 2. bfs 탐색으로 덩어리 구하기
+            boolean[] check = new boolean[n+1]; // 방문체크
+            ArrayList<Integer> d = new ArrayList<>();
+            for (int start = 1; start <= n; start++) {
+                if (!check[start]) {
+                    int top = bfs(start, check, a, 1);
+                    d.add(top);
+                }
+            }
+            answer = Math.min(answer, Math.abs(d.get(0)-d.get(1)));
+        }
+        return answer;
+    }
+}
