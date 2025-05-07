847b4fa42e93a35946fe60c741171a1bb2090449
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2025-05-07 21:34:54 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.04 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -5,6 +5,7 @@ import java.util.*;
 // Arrays.copyRange() : 배열에서 사용 => 적합!
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        return Arrays.copyOfRange(num_list, n-1, num_list.length);
+        int[] answer = Arrays.copyOfRange(num_list, n-1, num_list.length);
+        return answer;
     }
 }
