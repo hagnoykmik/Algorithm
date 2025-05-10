6bf9f7bcfd3b2017bd87747d7e404924b83a9f3b
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2025-05-10 21:54:18 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 54.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+//         int[] d = new int[1000000000];
+//         d[0] = 0;
+        
+//         for (int k = 1; k <= n; k++) {
+//             if (k % 2 == 0) { // 순간이동 될 때(*2)
+//                 d[k] = d[k/2];
+//             } else { // K칸 앞으로 점프해야할 때
+//                 d[k] = d[k-1] + 1;
+//             }
+//         }
+        // return d[n];
+        int ans = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n -= 1;
+                ans += 1;
+            }
+        }
+        return ans;
+    }
+}
